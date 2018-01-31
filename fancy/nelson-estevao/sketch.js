83c47d4d81307361ef2c97dfcc68b28d366362c5
Nelson Estevão <nelsonmestevao@gmail.com>
2018-01-31 12:11:18 +0000

    Add nelson fancy :deciduous_tree: version

--- fancy/nelson-estevao/sketch.js
+++ fancy/nelson-estevao/sketch.js
@@ -4,30 +4,56 @@ var angle;
 var slider;
 
 function setup() {
-  createCanvas(800, 800);
-  slider = createSlider(0, PI, PI/4, 0.01);
+  createCanvas(1600, 900);
+  slider = createSlider(0, PI, PI/6, 0.01);
 }
 
 function draw() {
-  background(0, 100, 100);
+  background(240, 248, 255);
+
   angle = slider.value();
-  stroke(255);
-  translate(400, height);
-  branch(200);
+  stroke(178, 34, 34);
+  strokeWeight(11);
+  push();
+    translate(400, height);
+    branch(200,10, angle);
+  pop();
+
+  angle = slider.value() - PI/15;
+  stroke(178, 34, 34);
+  strokeWeight(11);
+  push();
+    translate(1200, height);
+    branch(200,10, angle);
+  pop();
 }
 
-function branch(len) {
+function branch(len,wei,ang) {
   line(0, 0, 0, -len);
   translate(0,-len);
+  strokeWeight(wei);
   if (len > 4) {
+    stroke(0, 191, 255);
     push();
-      rotate(angle);
-      branch(len * 0.7);
+      rotate(ang);
+      branch(len * 0.7, wei * 0.7, ang * .9);
+    pop();
+
+    stroke(95, 158, 160);
+    push();
+      rotate(-2*ang);
+      branch(len * 0.7, wei * 0.7, ang * 1.2);
+    pop();
+
+    stroke(100, 200, 100);
+    push();
+      rotate(2*ang);
+      branch(len * 0.3, wei * 0.7, ang * 1.1);
     pop();
 
     push();
-      rotate(-angle);
-      branch(len * 0.7);
+      rotate(-ang);
+      branch(len * 0.3, wei * 0.7, ang * 1.15);
     pop();
   }
 }
