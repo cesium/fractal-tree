9884dd63af9c2eff03267e99771af1c642378180
Nelson Estevão <nelsonmestevao@gmail.com>
2019-01-30 02:52:56 +0000

    Update starting point

--- begin/sketch.js
+++ begin/sketch.js
@@ -1,9 +1,11 @@
 // Fractal Trees
 
 function setup() {
-  createCanvas(400, 400);
+    createCanvas(400, 400);
+    angleMode(DEGREES); // if you prefer to work with degrees
 }
 
 function draw() {
-  background(42);
+    background(42);
+    stroke(100);
 }
